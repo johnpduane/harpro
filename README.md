9fd3930a12d51199624c624d036f42b6905e8f44
johnpduane <johnpduane@gmail.com>
2014-09-09 17:52:12 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 harpro
 ======
 
-Consumes related HAR and CPUPROFILE files to generate reports
+Harpro consumes related HAR and CPUPROFILE files to generate a report of the JavaScript downloaded but not used.  Harpro requires node.js.
+```
+harpro [--har] [--pro] [--verbose]
+```
+Harpro will process the files referenced in the --har or --pro options independently.
+If provided together, harpro will compare the usage of Javascript files referenced in the har file
+with usage of these files and functions in the cpuprofile file.
+```
+Options:
+  --har           - HAR file name to process, should be found within provided directory
+  --pro           - CPUPROFILE file name to process, should be found within provided directory
+  --verbose       - print tab separated value of all data processed
+  --version       - show the current version
+  -h, --help      - display this help and exit
+```
+For example:
+
+```
+$ node harpro www.yahoo.com.har www.yahoo.com.cpuprofile
+
+6 JavaScript files were downloaded but not used:
+File                                              Download Time
+https://s.yimg.com/.../widget-uievents_3.8.2.js   217.79990196228027
+https://s.yimg.com/.../carousel_service_0.1.62.js 98.33002090454102
+https://s.yimg.com/.../masthead_0.2.306.js        131.68001174926758
+https://s.yimg.com/.../lw_geo_0.0.1.js            28.339862823486328
+https://y.analytics.yahoo.com/fpc.pl?...          52.89006233215332
+
+8 JavaScript files were downloaded and used:
+File                                              CPU Time            Uses
+https://s.yimg.com/.../yfpad_timetracking.js      7.018125573486533   19
+https://s.yimg.com/rq/darla/2-8-1/js/g-r-min.js   49.126879014405745  67
+https://s.yimg.com/.../yui-base_3.8.3.js          28.072502293946133  48
+https://s.yimg.com/lq/lib/3pm/cs_0.2.js           2.005178735281867   6
+https://s.yimg.com/.../yui_service_0.1.19.js      31.080270396868933  58
+https://pr.comet.yahoo.com/comet?                 0	                  1
+https://s.yimg.com/.../dd-scroll-min.js           1.0025893676409334  23
+https://y.analytics.yahoo.com/fpc.pl              0                   1
+```
---
+++ harpro.js
@@ -0,0 +1,160 @@
+/*
+ Copyright 2014 John Duane. All rights reserved.
+ Use of this source code is governed by a BSD-style license that can be
+ found in the LICENSE file.
+ */
+
+var Profile = require('./lib/profile.js');
+var Har = require('./lib/har.js');
+var args = process.argv.slice(2);
+var profileFile = null;
+var harFile = null;
+var verbose = false;
+
+if (args.length == 0) {
+    help();
+}
+else {
+    while(args.length) {
+        var arg = args.shift();
+        switch(arg)
+        {
+            case '--version':
+                printVersion();
+                break;
+            case '--verbose':
+                verbose = true;
+                break;
+            case '--har':
+                harFile = args.shift();
+                break;
+            case '--pro':
+                profileFile = args.shift();
+                break;
+            case '-h':
+                help();
+                break;
+            default:
+                if (arg.slice(-4) === ".har") {
+                    harFile = arg;
+                }
+                else if (arg.slice(-11) === ".cpuprofile") {
+                    profileFile = arg;
+                }
+                else if (arg.match(/^--/)) {
+                    help();
+                }
+                break;
+        }
+    }
+    processInput();
+}
+
+function processInput() {
+    var har, pro;
+    try {
+        if (!profileFile && !harFile) {
+            console.log("You must specify either a --har or --pro option.");
+            help();
+        }
+
+        if (harFile && harFile !== "") {
+            har = new Har(harFile);
+        }
+
+        if (profileFile && profileFile !== "") {
+            pro = new Profile(profileFile);
+        }
+
+        if (har && pro) {
+            console.log(compareHarPro(har, pro));
+        }
+        else if (verbose) {
+            if (har) console.log(har.generateDetails());
+            if (pro) console.log(pro.generateDetails());
+        }
+        else {
+            if (har) console.log(har.generateSummary());
+            if (pro) console.log(pro.generateSummary());
+        }
+
+    }
+    catch (e) {
+        console.log(e.message + '\n');
+        help();
+    }
+}
+
+function compareHarPro(har, pro) {
+    var i,
+        found = [],
+        unfound = [],
+        profileInfo,
+        output = "";
+    for (i = 0; i < har.harJs.entries.length; i++) {
+        profileInfo = pro.getUrlProfileInformation(har.harJs.entries[i].url);
+        if (profileInfo[har.harJs.entries[i].url]) {
+            found.push({
+                harEntry: har.harJs.entries[i],
+                profile: profileInfo[har.harJs.entries[i].url]
+            });
+        }
+        else {
+            unfound.push(har.harJs.entries[i]);
+        }
+    }
+
+    //unfound
+    if (unfound.length === 0) {
+        output = "All JavaScript files downloaded were used.\n";
+    }
+    else {
+        output = unfound.length + " JavaScript files were downloaded but not used:\n";
+        output += "File\tDownload Time\n";
+        for (i = 0; i < unfound.length; i++) {
+            output += unfound[i].url + "\t" +
+                      unfound[i].time+ "\n";
+        }
+    }
+
+    //found
+    output += "\n";
+    if (found.length === 0) {
+        output += "None of the JavaScript files downloaded were used.\n";
+    }
+    else {
+        output += found.length + " JavaScript files were downloaded and used:\n";
+        output += "File\tCPU Time\tUses\n";
+        for (i = 0; i < found.length; i++) {
+            output += found[i].harEntry.url + "\t" +
+                      found[i].profile.time + "\t" +
+                      found[i].profile.usageCount + "\n";
+        }
+    }
+
+    return output;
+}
+
+function help(){
+    process.stdout.write([
+        'USAGE: harpro [--har] [--pro]'
+        , 'harpro will process the files referenced in the --har or --pro options independently.'
+        , 'If provided together, harpro will compare the usage of Javascript files referenced in the har file'
+        , 'with usage of these files and functions in the cpuprofile file.'
+        , ''
+        , 'Options:'
+        , '  --har           - HAR file name to process, should be found within provided directory'
+        , '  --pro           - CPUPROFILE file name to process, should be found within provided directory'
+        , '  --verbose       - print tab separated value of all data processed'
+        , '  --version       - show the current version'
+        , '  -h, --help      - display this help and exit'
+        , ''
+    ].join("\n"));
+
+    process.exit(-1);
+}
+
+function printVersion(){
+    console.log("1.0.0");
+    process.exit(0);
+}
